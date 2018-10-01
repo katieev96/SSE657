7ad7f0fd6eab9a0676a384168e61858b383fde62
Katie Elizabeth Vick <10608995@live.mercer.edu>
2018-10-01 18:25:36 -0400

    Simple program

---
+++ RentalCompany/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RentalCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
